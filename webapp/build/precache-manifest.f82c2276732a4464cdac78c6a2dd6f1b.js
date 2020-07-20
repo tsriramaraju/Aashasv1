44b6c252fb95ed319a83b9cbc82801716de0ddf4
tsriramaraju <tsriramaraju@gmail.com>
2020-07-20 20:39:29 +0530

    ready for push

---
+++ webapp/build/precache-manifest.f82c2276732a4464cdac78c6a2dd6f1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf86179dbebdda250ed680632bd259c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab9cf249cbcfb646eb72",
+    "url": "/static/css/main.4f414518.chunk.css"
+  },
+  {
+    "revision": "2942a37ff16bf0c51c00",
+    "url": "/static/js/2.1abacffa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1abacffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9cf249cbcfb646eb72",
+    "url": "/static/js/main.052f2d07.chunk.js"
+  },
+  {
+    "revision": "b3cfd9f5cdf691e24e2f",
+    "url": "/static/js/runtime-main.00ed32f5.js"
+  },
+  {
+    "revision": "dbcfd97858e675726b324261a3ca2c3f",
+    "url": "/static/media/spinner.dbcfd978.gif"
+  }
+]);
